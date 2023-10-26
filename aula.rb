94fab3177b3fa06ac53b14c91b9c774dbfaa7019
Maria Sanchez <clarasanchez2398@gmail.com>
2023-10-26 08:18:01 -0300

    first commit

---
+++ aula.rb
@@ -0,0 +1,13 @@
+puts "Hello word"
+
+# Saída e entrada de dados
+print "Digite seu nome: "
+nome = gets.chomp
+
+puts "Oi #{nome}!"
+
+#Leitura de numbers
+num1 = gets.chomp.to_i
+num2 = gets.chomp.to_i
+
+puts "Soma = #{num1+num2}"
---
+++ desafio.rb
@@ -0,0 +1,8 @@
+print "Digite seu nome: "
+nome = gets.chomp
+print "Digite seu Sobrenome: "
+sobrenome = gets.chomp
+print "Digite sua idade: "
+idade = gets.chomp.to_i
+
+puts "#{nome} #{sobrenome}. Idade = #{idade}"
